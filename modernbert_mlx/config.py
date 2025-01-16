196a9b9f6c72bfb4c0e58e55bce0bca5057a2b40
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2025-01-15 23:54:53 -0800

    prepare-for-mlx-lm

--- modernbert_mlx/config.py
+++ modernbert_mlx/config.py
@@ -1,6 +1,45 @@
 from enum import Enum
+from typing import Literal, Optional
 
 from pydantic import BaseModel, Field
+
+
+# parity with HuggingFace version
+class ModernBERTConfig(BaseModel):
+    vocab_size: int = 50368
+    hidden_size: int = 768
+    intermediate_size: int = 1152
+    num_hidden_lauers: int = 22
+    hidden_activation: str = "gelu"
+    max_position_embeddings: int = 8192
+    initializer_range: float = 0.02
+    initializer_cutoff_factor: float = 2.0
+    norm_eps: float = 1e-5
+    norm_bias: bool = False
+    pad_token_id: int = 50283
+    eos_token_id: int = 50282
+    bos_token_id: int = 50281
+    cls_token_id: int = 50281
+    sep_token_id: int = 50282
+    global_rope_theta: float = 160000.0
+    attention_bias: int = False
+    attention_dropout: float = 0.0
+    global_attn_every_n_layers: int = 3
+    local_attention: int = 128
+    local_rope_theta: float = 10000.0
+    embedding_dropout: float = 0.0
+    mlp_bias: bool = False
+    mlp_dropout: float = 0.0
+    decoder_bias: bool = True
+    classifier_pooling: Literal["cls", "mean"] = "cls"
+    classifier_dropout: float = 0.0
+    classifier_bias: bool = False
+    classifier_activation: str = "gelu"
+    deterministic_flash_attn: bool = False
+    sparse_prediction: bool = False
+    sparse_pred_ignore_index: int = -100
+    reference_compile: Optional[bool] = None
+    repad_logits_with_grad: bool = False
 
 
 class AttentionImpl(str, Enum):
